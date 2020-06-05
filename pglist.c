0db9851f3a5032d6fe8f71ab4993f5c376214d4f
심건우 <kenwooshim@gmail.com>
2020-06-05 16:47:57 +0900

    First commit

--- pglist.c
+++ pglist.c
@@ -2,21 +2,25 @@
 #include <string.h>
 #include "page.h"
 
-static struct frame *free_list;
+static struct frame *free_list;//물리기억장치?
 // free frame list
-struct frame * get_free_frame() 
+struct frame * get_free_frame() // demand page 발생시
 {
-	return NULL;
+	// free_frame을 하나 가져온다.
+	//gunwoo's code start!
+	free_list =free_list->n;
+	return free_list->p;
 }
 
-void page_init() 
+void page_init() //free_list를 초기화 한다
 {
 	struct frame *p;
 	struct frame *n;
 
 	// initialize pg_frames
 	memset(page_frame, 0, sizeof(page_frame)); 
-
+	
+	//page_frame에 있는 frame들을 앞뒤로 연결하는 작업
 	for (int i = 0 ; i < MAX_FRAMES ; i++) {
 		// fill in free page frames list
 		page_frame[i].pfn = i;
--- vm.c
+++ vm.c
@@ -1,4 +1,3 @@
-
 // virtual memory simulation
 // gets input from input_vm
 // prints output va -> pa
@@ -9,20 +8,33 @@
 #include "page.h"
 
 // assume Pmem 1MB
+// PMem = 4byte 짜리로 1MB 개만듬.
 int PMem[0x100000];
 
+//PMem의 i인덱스 값을 i로 초기화 해주는 함수
 void init_pmem()
 {
 	int i;
 	for (i = 0 ; i < 0x100000 ; i++) {
-		PMem[i] = i;
+		PMem[i] = i;// PMem[0]=0, PMem[4]=4
 	}
 }
 
-void access_pa(int pid, int va, int pa)
+//얘는 뭐하는 놈이지?
+int access_pa(int pid, int va)
 {
+	int pa=0;
+	int l1 = va * (0x11110000);//table index
+	int offset = va * (0x111);
 
+	if(pg_table[0][l1] = -1 )//table에 index가 존재하는지?
+		printf("Demand paging 요구가 발생하였습니다.");
+		pg_table[0][l1] = get_free_frame()->pfn; // Demand paging
+	pa = l1+offset;
+	return pa;
 }
+
+
 
 void main(int argc, char *argv[])
 {
@@ -47,17 +59,21 @@ void main(int argc, char *argv[])
 	}
 	fp = fopen(input_file_name, "r");
 
+	//pglist.c에 구현되있음
 	page_init();
 	init_pmem();
 
 	// get a line from input
-	while (getline(&line, &len, fp) != -1) {
+	while (getline(&line, &len, fp) != -1) {//한줄씩 읽는다.
 		pid_str = strtok_r(line, " \n", &saveptr);
 		va_str = strtok_r(NULL, " \n", &saveptr);
 
 		pid = strtol(pid_str, &e, 16);
 		va = strtol(va_str, &e, 16);
-		pa = 0; // needs to be translated from va
+		//여기가 중요!
+		pa = access_pa(pid,va);
+
+
 		printf("pid: %d, va: 0x%08x pa: [0x%08x] = 0x%08X\n", pid, va, pa, PMem[pa]);
 	}
 
