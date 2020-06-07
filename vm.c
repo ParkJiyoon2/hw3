16825d477b9961d554f829f5de4ed4d2387a9a29
gudwnsepdy <gudwnsepdy@naver.com>
2020-06-08 01:38:18 +0900

    first

--- vm.c
+++ vm.c
@@ -1,32 +1,36 @@
-
-// virtual memory simulation
-// gets input from input_vm
-// prints output va -> pa
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "page.h"
 
-// assume Pmem 1MB
+
 int PMem[0x100000];
+//static struct frame *free_list;
+
 
 void init_pmem()
 {
 	int i;
-	for (i = 0 ; i < 0x100000 ; i++) {
+	for (i = 0; i < 0x100000; i++) {
 		PMem[i] = i;
 	}
 }
 
-int access_pa(int pid, int va)
-{
 
+void access_pa(int pid, int va) {
+	int pa;
+	if (pg_table[NPROC][va >> 12] == -1) {		
+		pg_table[NPROC][va >> 12] = get_free_frame() -> p -> pfn;		
+		printf("^11234^fault! freepfn: 0x%x pgn: 0x%x ", pg_table[NPROC][va >> 12], va >> 12);
+												
+	}
+	pa = (pg_table[NPROC][va >> 12]<<12) + (va & 0x00000fff);	
+	printf("8pid: %d, va: 0x%08x pa: [0x%08x] = 0x%08X\n", pid, va, pa, PMem[pa]);
 }
 
 void main(int argc, char *argv[])
 {
-	char *input_file_name = "input_vm";;
+	char *input_file_name = "input_vm";;	
 	FILE *fp; //input file ptr
 	char *line = NULL;
 	char *saveptr;
@@ -37,29 +41,30 @@ void main(int argc, char *argv[])
 	int va;
 	int pa;
 	char *e;
+	
+	size_t len;		
 
-	int pg;
-	int pg_offset;
-	size_t len;
-
-	if (argc == 2) {
-		input_file_name = argv[1];
+	if (argc == 2) {		
+		input_file_name = argv[1];		
 	}
-	fp = fopen(input_file_name, "r");
+	fp = fopen(input_file_name, "r");	
 
-	page_init();
-	init_pmem();
+	page_init();		
+	init_pmem();		
 
-	// get a line from input
+	// init page table
+	//pg_table[NPROC][PG_TBL_SIZE] 
+	for(int i = 0; i<PG_TBL_SIZE; i++){
+	memset(pg_table[i], -1, sizeof(NPROC) * PG_TBL_SIZE );
+	}
+	
 	while (getline(&line, &len, fp) != -1) {
-		pid_str = strtok_r(line, " \n", &saveptr);
-		va_str = strtok_r(NULL, " \n", &saveptr);
-
-		pid = strtol(pid_str, &e, 16);
-		va = strtol(va_str, &e, 16);
-		pa = 0; // needs to be translated from va
-		printf("pid: %d, va: 0x%08x pa: [0x%08x] = 0x%08X\n", pid, va, pa, PMem[pa]);
+		pid = strtol(strtok_r(line, " \n", &saveptr), &e, 16);	
+		va = strtol(strtok_r(NULL, " \n", &saveptr), &e, 16);	
+		access_pa(pid, va);	
 	}
 
 	fclose(fp);
 }
+
+
